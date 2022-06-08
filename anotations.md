9e95f25b4065888e4ef50280753ac94a2362a37c
fraanpsilva <francilenesilva.pt@gmail.com>
2022-06-08 22:14:07 +0100

    recolvendo promessas

--- anotations.md
+++ anotations.md
@@ -4,4 +4,6 @@ node é um runtime, permite que o js seja executado fora do navegador
 [npm](https://docs.npmjs.com/)
 [yarn](https://yarnpkg.com/)
 [Markdown](https://www.markdownguide.org/getting-started/)
+[Promessas](https://www.alura.com.br/artigos/async-await-no-javascript-o-que-e-e-quando-usar)
+[New Promise](https://cursos.alura.com.br/course/nodejs-criando-biblioteca/task/97389)
 
--- index.js
+++ index.js
@@ -6,7 +6,32 @@ function trataErro(error){
     throw new Error(chalk.red(error.code, 'não há arquivo no caminho'));
 }
 
+/*// 1ª forma de resolver promessas
 function pegaArquivo(pathFile){
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(pathFile, encoding)
+        .then((text) => chalk.green(console.log(text)))
+        .catch((err) => trataErro(err))
+}*/
+
+// 2ª forma de resolcer promessas
+async function pegaArquivo(pathFile){
+    const encoding = 'utf-8';
+
+    try{
+        
+        const text = await fs.promises.readFile(pathFile, encoding)
+        console.log(chalk.green(text))
+    } catch(err) {
+        trataErro(err)
+    }
+    
+    
+}
+
+// forma  síncrona
+/*function pegaArquivo(pathFile){
     const encoding = 'utf-8';
     fs.readFile(pathFile, encoding, (err, text) => {
         if(err){
@@ -14,6 +39,6 @@ function pegaArquivo(pathFile){
         }
         console.log(chalk.green(text));
     } );
-}
+}*/
 
-pegaArquivo('./arquivos/texto1.md');
+pegaArquivo('./arquivos/');
