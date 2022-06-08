72c46e14abd74fa43b09746e9c09f6665004db77
fraanpsilva <francilenesilva.pt@gmail.com>
2022-06-08 17:49:10 +0100

    Create project

---
+++ index.js
@@ -0,0 +1,14 @@
+import chalk from 'chalk';
+//const chalk = require('chalk');
+
+
+console.log(chalk.blue('Vamos começar!'));
+
+
+const paragrafo = 'Texto retornado por uma função';
+
+function texto(string){
+    return string;
+}
+
+console.log(texto(paragrafo));
