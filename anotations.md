6bf6c984ab07d7842b6b9451aa6e2497aa2ddf46
fraanpsilva <francilenesilva.pt@gmail.com>
2022-06-08 18:19:52 +0100

    lib fs e tratamento de erros

--- anotations.md
+++ anotations.md
@@ -1,7 +1,7 @@
 node é um runtime, permite que o js seja executado fora do navegador
 
-* npm init -> iniciando um novo projeto
-(npm)[https://docs.npmjs.com/]
-(yarn)[https://yarnpkg.com/]
-(Markdown)[https://www.markdownguide.org/getting-started/]
-(ESM vs CJS)[https://cursos.alura.com.br/course/nodejs-criando-biblioteca/task/96840]
+* npm init -> iniciando um novo projeto </br>
+[npm](https://docs.npmjs.com/)
+[yarn](https://yarnpkg.com/)
+[Markdown](https://www.markdownguide.org/getting-started/)
+
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import chalk from 'chalk';
-//const chalk = require('chalk');
+import fs from 'fs';
 
 
-console.log(chalk.blue('Vamos começar!'));
-
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string){
-    return string;
+function trataErro(error){
+    throw new Error(chalk.red(error.code, 'não há arquivo no caminho'));
 }
 
-console.log(texto(paragrafo));
+function pegaArquivo(pathFile){
+    const encoding = 'utf-8';
+    fs.readFile(pathFile, encoding, (err, text) => {
+        if(err){
+            trataErro(err);
+        }
+        console.log(chalk.green(text));
+    } );
+}
+
+pegaArquivo('./arquivos/texto1.md');
