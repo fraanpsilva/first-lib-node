cd5768032360c96d95d901bef24dac194b6c83a0
fraanpsilva <francilenesilva.pt@gmail.com>
2022-06-09 11:04:31 +0100

    Expressões regulares

--- anotations.md
+++ anotations.md
@@ -1,9 +1,15 @@
 node é um runtime, permite que o js seja executado fora do navegador
 
 * npm init -> iniciando um novo projeto </br>
-[npm](https://docs.npmjs.com/)
-[yarn](https://yarnpkg.com/)
-[Markdown](https://www.markdownguide.org/getting-started/)
-[Promessas](https://www.alura.com.br/artigos/async-await-no-javascript-o-que-e-e-quando-usar)
-[New Promise](https://cursos.alura.com.br/course/nodejs-criando-biblioteca/task/97389)
 
+
+* [npm](https://docs.npmjs.com/)
+* [yarn](https://yarnpkg.com/)
+* [Markdown](https://www.markdownguide.org/getting-started/)
+* [Promessas](https://www.alura.com.br/artigos/async-await-no-javascript-o-que-e-e-quando-usar)
+* [New Promise](https://cursos.alura.com.br/course/nodejs-criando-biblioteca/task/97389)
+* [Strings](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String#)
+* [RegEx](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp)
+* [Validações de formulários - Joi](https://joi.dev/api/?v=17.6.0)
+* [Validações de formulários - yup](https://www.npmjs.com/package/yup)
+* [Ambiente de testes joi](https://joi.dev/tester/)
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+// estraindo links do texto
+function extraiLinks(text){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
 
+    while((temp = regex.exec(text)) !== null ){
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+    return arrayResultados;
+}
+
+// 2ª forma de resolcer promessas
+async function pegaArquivo(pathFile){
+    const encoding = 'utf-8';
+
+    try{ 
+        const text = await fs.promises.readFile(pathFile, encoding)
+        console.log(extraiLinks(text));
+    } catch(err) {
+        trataErro(err)
+    }   
+}
+
+// tratando erro 
 function trataErro(error){
     throw new Error(chalk.red(error.code, 'não há arquivo no caminho'));
 }
+
+pegaArquivo('./arquivos/texto1.md');
+
+
+
+
+
 
 /*// 1ª forma de resolver promessas
 function pegaArquivo(pathFile){
@@ -15,20 +46,6 @@ function pegaArquivo(pathFile){
         .catch((err) => trataErro(err))
 }*/
 
-// 2ª forma de resolcer promessas
-async function pegaArquivo(pathFile){
-    const encoding = 'utf-8';
-
-    try{
-        
-        const text = await fs.promises.readFile(pathFile, encoding)
-        console.log(chalk.green(text))
-    } catch(err) {
-        trataErro(err)
-    }
-    
-    
-}
 
 // forma  síncrona
 /*function pegaArquivo(pathFile){
@@ -41,4 +58,5 @@ async function pegaArquivo(pathFile){
     } );
 }*/
 
-pegaArquivo('./arquivos/');
+
+
